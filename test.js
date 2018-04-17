04481ba42462c5d77fbc29e5730670cab1bc8b53
Iago Bruno <httpiago@gmail.com>
2018-04-17 05:51:42 -0300

    Deixar mais legível

--- test.js
+++ test.js
@@ -2,19 +2,24 @@ const assert = require('assert');
 
 var getUsers = () => [{name: 'Anna Wintour'}, {name: 'Fierce'}];
 var gAuth = (obj) => 'mskpjoaJNJASN.WJ098IWM12022822.WKNSJKNWJKNB229202.W';
+var test = (boo) => assert.ok(boo);
 
-describe('Testando a API', function () {
+context('Testando a API', function () {
+
   it('Buscar usuários', function () {
-    assert.equal( typeof getUsers(), 'object');
+    test(typeof getUsers() === 'object');
   });
 
   it('Conectar ao Socket', function() {
     let connected = true;
-    assert.equal(connected, true);
+
+    test(connected === true);
   });
+
 });
 
-describe('Conexão com o Google Cloud', function () {
+context('Conexão com o Google Cloud', function () {
+
   it('Login', function () {
     let token = gAuth({user: 'Banks', pass: '...'});
     assert.ok(true);
@@ -23,4 +28,5 @@ describe('Conexão com o Google Cloud', function () {
   it('Enviar dados', () => assert.ok(true));
 
   it('Verificar se a outra conexão não existe mais', () => assert.notEqual(true, false));
+
 });
